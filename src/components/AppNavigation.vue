2f40b64e81dd667d3caebae7f33448aa33e6bf35
Joana Bencatel <bencateljoana@gmail.com>
2020-07-11 12:27:38 +0100

    Made the home page accessible throught the app header title

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,47 +1,54 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" app>
+    <v-app-bar app color="indigo" dark clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title style="cursor: pointer" @click="$router.push('/')">
+        Lapas Loggers
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
-        <v-list-item link :to="{ name: 'Home' }">
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link :to="{ name: 'About' }">
-          <v-list-item-action>
-            <v-icon>mdi-information</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>About</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-email</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+        <template v-for="(item, i) in items">
+          <v-row v-if="item.heading" :key="i" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-right">
+              <v-btn small text>edit</v-btn>
+            </v-col>
+          </v-row>
+          <v-divider v-else-if="item.divider" :key="i" dark class="my-1" />
+          <v-list-item v-else :key="i" link :to="item.link">
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title class="grey--text">
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
       </v-list>
     </v-navigation-drawer>
-
-    <v-app-bar app color="indigo" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Lapas Loggers</v-toolbar-title>
-    </v-app-bar>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    drawer: false
+    drawer: true,
+    items: [
+      { icon: "mdi-beach", text: "Shores", link: "/shores" },
+      { icon: "mdi-leak", text: "Loggers", link: "/loggers" },
+      { icon: "mdi-calendar-refresh", text: "Servicings", link: "/servicings" },
+      { icon: "mdi-account-group", text: "People", link: "/people" },
+      { divider: true },
+      { icon: "mdi-information", text: "About", link: "/about" }
+    ]
   })
 };
 </script>
