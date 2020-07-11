5d4a083294ab9ed9099c49822ee9e611ac5527c3
Joana Bencatel <bencateljoana@gmail.com>
2020-07-11 18:59:19 +0100

    Improved the Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <template>
-  <v-row align="center" justify="center" style="height: 100%">
-    <h1>Welcome to the Lapas Loggers Project</h1>
-  </v-row>
+  <v-container fluid fill-height class="home-hero">
+    <v-row justify="center" align="center" column pa-5 wrap class="text-center">
+      <v-col
+        cols="12"
+        class="display-3 font-weight-black white--text text-xs-center"
+      >
+        Welcome to
+      </v-col>
+      <v-col
+        cols="12"
+        class="display-3 font-weight-black white--text text-xs-center mb-3"
+      >
+        LAPAS LOGGERS PROJECT
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -11,3 +24,12 @@ export default {
   components: {}
 };
 </script>
+
+<style scoped>
+.home-hero {
+  background: url("https://upload.wikimedia.org/wikipedia/commons/f/f4/Rocky_beach.jpg");
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
