f29c381749ba4abbd21577140288ad3f579b3c41
Joana Bencatel <bencateljoana@gmail.com>
2020-07-20 20:03:19 +0100

    Placed the Shore Edit Form in a dedicated component

---
+++ src/components/pages/Management/Shores/ShoreEditForm.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <span class="headline">{{ formTitle }}</span>
+    </v-card-title>
+
+    <v-card-text>
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field v-model="editedItem.name" label="Shore name" />
+          </v-col>
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field v-model="editedItem.code" label="Code" />
+          </v-col>
+          <v-col cols="12" sm="6" md="4">
+            <v-autocomplete
+              v-model="editedItem.country_id"
+              :items="countries"
+              item-text="name"
+              item-value="id"
+              label="Country"
+            />
+          </v-col>
+          <v-col cols="12" sm="6" md="4">
+            <v-autocomplete
+              v-model="editedItem.fwunit_id"
+              label="Field Work Unit"
+              :items="fieldWorkUnits"
+              item-text="name"
+              item-value="id"
+            />
+          </v-col>
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field
+              v-model="editedItem.latitude"
+              label="Latitude"
+              type="number"
+            />
+          </v-col>
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field
+              v-model="editedItem.longitude"
+              label="Longitude"
+              type="number"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="blue darken-1" text @click="$emit('close')">Cancel</v-btn>
+      <v-btn color="blue darken-1" text @click="$emit('save')">Save</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ShoreEditForm",
+
+  props: {
+    formTitle: {
+      type: String,
+      required: true
+    },
+    editedItem: {
+      type: Object,
+      required: true
+    },
+    countries: {
+      type: Array,
+      required: true
+    },
+    fieldWorkUnits: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/components/pages/Management/Shores/ShoresList.vue
+++ src/components/pages/Management/Shores/ShoresList.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
+  <v-container>
+    <v-toolbar flat color="white">
+      <v-toolbar-title>Shores</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-dialog v-model="dialog" max-width="500px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+            New Item
+          </v-btn>
+        </template>
+        <shore-edit-form
+          :edited-item="editedItem"
+          :form-title="formTitle"
+          :countries="countries"
+          :field-work-unitss="fieldWorkUnits"
+          @close="close"
+          @save="save"
+        />
+      </v-dialog>
+    </v-toolbar>
+
     <v-data-table
       :headers="headers"
       :items="shores"
@@ -7,82 +29,6 @@
       class="elevation-1"
       @click:row="openItem"
     >
-      <template v-slot:top>
-        <v-toolbar flat color="white">
-          <v-toolbar-title>Shores</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
-
-          <v-spacer />
-
-          <v-dialog v-model="dialog" max-width="500px">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-                New Item
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="headline">{{ formTitle }}</span>
-              </v-card-title>
-
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.name"
-                        label="Shore name"
-                      />
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.code" label="Code" />
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-autocomplete
-                        v-model="editedItem.country_id"
-                        :items="countries"
-                        item-text="name"
-                        item-value="id"
-                        label="Country"
-                      />
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-autocomplete
-                        v-model="editedItem.fwunit_id"
-                        label="Field Work Unit"
-                        :items="fieldWorkUnits"
-                        item-text="name"
-                        item-value="id"
-                      />
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.latitude"
-                        label="Latitude"
-                        type="number"
-                      />
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.longitude"
-                        label="Longitude"
-                        type="number"
-                      />
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-
       <template v-slot:item.actions="{ item }">
         <v-icon small class="mr-2" @click.stop="editItem(item)">
           mdi-pencil
@@ -96,13 +42,17 @@
         <v-btn color="primary" @click="initialize">Reset</v-btn>
       </template>
     </v-data-table>
-  </div>
+  </v-container>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import ShoreEditForm from "./ShoreEditForm";
+
 export default {
   name: "ManageShoresList",
+
+  components: { ShoreEditForm },
 
   data: () => ({
     dialog: false,
